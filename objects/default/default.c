69c8e17b0ae0fad44cc435a84e6c09efe5edbf96
- <niko@s-MacBook-Air.home>
2023-04-30 22:06:23 -0400

    Adding more placeholder files

---
+++ objects/default/default.c
@@ -0,0 +1,28 @@
+//
+// Default Object
+//
+
+// Create event for the initialization phaze
+create() {
+
+}
+
+// Step event for actions every frame
+step(deltatime) {
+
+}
+
+// Draw event for drawing stuff :3
+draw(deltatime) {
+	
+}
+
+// Clean event for room changes or game endings
+clean() {
+
+}
+
+// Destroy event in the case the object gets deleted
+destroy() {
+
+}
